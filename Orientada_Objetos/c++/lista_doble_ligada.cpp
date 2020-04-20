e8e6323543e113c42c5cd381aa85ce2802bacbb7
WilverC <jcortezen09@hotmail.com>
2020-04-20 14:58:57 -0500

    lista_doble_ligada

--- Orientada_Objetos/c++/lista_doble_ligada.cpp
+++ Orientada_Objetos/c++/lista_doble_ligada.cpp
@@ -16,6 +16,7 @@ class nodo{
 public:
 	int valor; //valor contenedor
 	nodo *siguiente; //el apuntador al siguiente nodo
+	nodo *anterior; //apunta al nodo anterior
 	nodo(int); //constructor 
 };
 
@@ -36,6 +37,7 @@ public:
 	void otro(int x); //coloca el elemento en cualquier posicion que no es el inicio
 	void mostrar(); //muestra el contenido de toda la lista
 	void siguiente(); //mueve el apuntador del usuario a la siguiente posicion
+	void anterior(); //mueve el apuntador del usuario al elemento anterior
 	void borrar(); //borra el nodo de la lista
 	void ve_inicio(); //mueve el apuntador del usuario al inicio de la lista
 	void ve_fin(); // mueve el apuntador del usuario al final de la lista
@@ -58,10 +60,11 @@ void lista::menu(){
 		cout<<"1. INSERTAR\n";
 		cout<<"2. MOSTRAR\n";
 		cout<<"3. SIGUIENTE\n";
-		cout<<"4. BORRAR\n";
-		cout<<"5. PRIMERO\n";
-		cout<<"6. ULTIMO\n";
-		cout<<"7. SALIR\n";
+		cout<<"4. ANTERIOR\n";
+		cout<<"5. BORRAR\n";
+		cout<<"6. PRIMERO\n";
+		cout<<"7. ULTIMO\n";
+		cout<<"8. SALIR\n";
 
 		cout<<"Elige una opcion: ";
 		cin>>x;
@@ -77,15 +80,18 @@ void lista::menu(){
 				siguiente();
 				break;
 			case 4:
-				borrar();
+				anterior();
 				break;
 			case 5:
-				ve_inicio();
+				borrar();
 				break;
 			case 6:
-				ve_fin();
+				ve_inicio();
 				break;
 			case 7:
+				ve_fin();
+				break;
+			case 8:
 				system("cls");
 				cout<<"*********************\n";
 				cout<<"Hasta luego\n";
@@ -99,7 +105,7 @@ void lista::menu(){
 				system("pause");
 		}
 
-	}while(x != 7);
+	}while(x != 8);
 }
 
 void lista::insertar(){
@@ -116,6 +122,7 @@ void lista::insertar(){
 void lista::primero(int x){
 	nodo *i = new nodo(x); //crea un nodo, asigna memoria
 	i->siguiente = NULL; // apunta a nulo el nodo siguiente
+	i->anterior = NULL; //apunta a nulo el nodo anterior
 	inicio = i; //los apuntadores inicio, fin y usuario apuntaran al primer nodo
 	fin = i;
 	usuario = i;
@@ -125,12 +132,15 @@ void lista::otro(int x){
 	nodo *i = new nodo(x); //crea un nuevo nodo
 	if(usuario = fin){ //verifica si el usuario se encuentra al final de la lista
 		i->siguiente = NULL; //siguiente nodo apuntara a nulo
+		i->anterior = fin; // su predecesor sera fin
 		fin->siguiente = i; //el siguiente de fin sera i
 		fin = i; //actualiza el apuntador fin al nuevo nodo
 		usuario = fin; //coloca al usuario en el nodo insertado
 	}else{//si no esta al final de la lista
 		i->siguiente = usuario->siguiente; //el siguiente del neuvo nodo es el siguiente donde esta el usuario
+		usuario->siguiente->anterior = i; //predecesor del siguiente de usuario sera el nuevo nodo
 		usuario->siguiente = i; //el siguiente apuntador de usuario es el nuevo nodo
+		i->anterior = usuario; //el anterior del nuevo nodo sera el usuario
 		usuario = i; //ubicamos al usuario en el nuevo nodo
 	}
 }
@@ -152,32 +162,49 @@ void lista::siguiente(){
 	if(usuario->siguiente != NULL){ //si hay nodo siguiente
 		usuario = usuario->siguiente; //actualizo el apuntador del usuario
 		cout<<"valor "<< usuario->valor<<"\n"; //imprime el valor
-		system("pause");
 	}else{ //no hay siguiente nodo
 		cout<<"esta en el ultimo elemento";
 		cout<<"valor "<<usuario->valor<<"\n";
-		system("pause");
 	}
+	system("pause");
+}
+
+void lista::anterior(){
+	system("cls");
+	if(usuario->anterior == NULL){ //si no hay nodo anterior
+		cout<<"Esta en el primer elemento";
+		cout<<"valor "<<usuario->valor<<"\n";
+	}else{ //no hay siguiente nodo
+		usuario = usuario->anterior;
+		cout<<"elemento anterior: "<<usuario->valor<<"\n";
+	}
+	system("pause");
 }
 
 void lista::borrar(){
 	nodo *temp;
 	system("cls");
-	if(usuario->siguiente == NULL){ //borra el ultimo elemento
-		cout<<"No hay elemento siguiente\n";
-	}else{ //borra el elemento en que esta el usuario
-		if(usuario->siguiente->siguiente == NULL){ //borrando el ultimo
-			cout<<"elemento borrado: "<< usuario->siguiente->valor;
-			delete usuario->siguiente; //borra el ultimo nodo
-			usuario->siguiente = NULL; //asigna nulo a siguiente
-			fin = usuario; //actualiza el apuntador de fin a usuario
-		}else{ //borra el nodo donde esta el usuario
-			temp = usuario->siguiente->siguiente; //asigna a al nodo temporal el nodo siguiente del nodo a borar
-			delete usuario->siguiente; //se borra el nodo seguido del usuario
-			usuario->siguiente = temp; //se actualiza el nodo siguiente del usuario
-		}
-		system("pause");
+	cout<<"Elemento a borrar: "<<usuario->valor<<"\n";
+	if(usuario == inicio){ //borra el primero
+		inicio = usuario->siguiente; //inicio va a ser igual al siguiente del inicio
+		inicio->anterior = NULL; //el anterior de inicio va a valer nulo
+		usuario = inicio; //y el usuario se queda en el primer elemento
+		delete usuario->anterior; //borro el nodo
 	}
+	else if(usuario == fin){ //borra el ultimo elemento
+		fin = usuario->anterior; //el nuevo fin sera la posicion del usuario
+		fin->siguiente = NULL; //el siguiente de fin es nulo
+		usuario = fin; //usuario sera igual a fin
+		delete usuario->siguiente; //borro el ultimo elemento
+	}
+	else{
+		usuario->anterior->siguiente = usuario->siguiente; //el siguiente del anterior del usuario sera el siguiente de usuario
+		usuario->siguiente->anterior = usuario->anterior; //el anterior del usuario siguientesera el anterior del usuario
+		temp = usuario->anterior; //se guarda el nodo siguiente a usuario
+		delete usuario; //borra usuario
+		usuario = temp; //
+	}
+	system("pause");
 }
 
 void lista::ve_inicio(){
